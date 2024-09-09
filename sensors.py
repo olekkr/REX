5060468ccbcdcaf1276647640de37bff220052de
ShatinNguyen <shatin2@hotmail.com>
2024-09-09 15:57:58 +0200

    ret

--- sensors.py
+++ sensors.py
@@ -7,9 +7,13 @@ print("Running")
 
 while 1:
     #print(arlo.read_sensor(0))
-    print("R:" + arlo.read_right_ping_sensor())
-    print("L:" + arlo.read_left_ping_sensor())
-    t.sleep(0.2)
+    print("R")
+    print(arlo.read_right_ping_sensor())
+    #print(arlo.read_left_ping_sensor())
+    t.sleep(1)
+    print("L")
+    print(arlo.read_left_ping_sensor())
+    t.sleep(1)
 
 
 # Front
