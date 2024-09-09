5326604f4eede3761f2cf0fc7bdfa3e3aea12c99
ShatinNguyen <shatin2@hotmail.com>
2024-09-09 15:53:18 +0200

    2 sensor

--- sensors.py
+++ sensors.py
@@ -7,5 +7,16 @@ print("Running")
 
 while 1:
     #print(arlo.read_sensor(0))
-    print(arlo.read_front_ping_sensor())
+    print(arlo.read_front_ping_sensor(), print(arlo.read_left_ping_sensor()), print(arlo.read_right_ping_sensor()))
     t.sleep(0.2)
+
+
+# Front
+# 10 cm, 130
+# 50 cm, 506
+# 100 cm, 1007
+# 150 cm, 1485
+# 200 cm, 1985 - 2000
+# 250 cm, 2472
+# 300 cm, 2971
+
