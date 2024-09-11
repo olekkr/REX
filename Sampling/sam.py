9d26353b1766d89c04fdbebd5fb88d467cdfe77d
ShatinNguyen <shatin2@hotmail.com>
2024-09-11 11:58:33 +0200

    ret

--- Sampling/sam.py
+++ Sampling/sam.py
@@ -7,19 +7,29 @@ interval = [0, 15]
 
 #e = 2.71828
 
-def N(x, p, q):
-    return (1 / np.sqrt(2 * np.pi) * q) *  np.e ** ((-0.5 * (x - p)**2) / (q ** 2))
+def N(x, mu, sigma):
+    return (1 / np.sqrt(2) * sigma) * np.e ** (-0.5 * (x - mu) ** 2 / sigma ** 2)
 
 def P(x):
-    return 0.3 * N(x, 2.0, 1.0) + 0.4 * N(x, 5.0, 2.0) + 0.3 * N(x, 9.0, 1.0)
+    return (0.3 * N(x, 2, 1) + 
+            0.4 * N(x, 5, 2) + 
+            0.3 * N(x, 9, 1))
 
-def Q(x):
-    return random.randint(interval[0], interval[1])
+def Q(x): 
+    rng = np.random.default_rng()
+    
+    
+print(Q(interval))
+
+for i in range(1):
+    x = np.arange(interval[0], interval[1], 0.01)
+    y = P(x)
+    #plt.hist(sample(i, interval))
+    plt.plot(x, y)
+    plt.show()
 
 
-
-
-x = np.arange(interval[0], interval[1])
-y = P(x)
-plt.plot(x, y)
-plt.show()
+# x = np.arange(interval[0], interval[1])
+# y = P(x)
+# plt.plot(x, y)
+# plt.show()
--- sensors.py
+++ sensors.py
@@ -5,20 +5,30 @@ arlo = robot.Robot()
 
 print("Running")
 
-while 1:
+i = 1
+while i:
     #print(arlo.read_sensor(0))
-    print("R")
-    print(arlo.read_sensor(1))
+    s1 = arlo.read_sensor(1)
     t.sleep(1)
+    s2 = arlo.read_sensor(1)
+    t.sleep(1)
+    s3 = arlo.read_sensor(1)
+    t.sleep(1)
+    s4 = arlo.read_sensor(1)
+    t.sleep(1)
+    s5 = arlo.read_sensor(1)
+    t.sleep(1)
+    print((s1 + s2 + s3 + s4 + s5) / 5)
+    i = 0
 
 
-# Front                 Right           Left                Back
-# 10 cm, 130            10 cm, 120      10 cm, 114          10, 130
-# 50 cm, 506            50 cm, 506      50 cm, 507          50, 517-518
-# 100 cm, 1007          100,1015        100, 1015           100, 1025-1028
-# 150 cm, 1485          150,1494        150, 1493           150, 1487
-# 200 cm, 1985 - 2000   200,1994        200, 1988           200, 1993
-# 250 cm, 2472          250,2479        250, 2480           250, 2476
-# 300 cm, 2971          300,2973        300, 2970           300, 2975 or 2608
+#       Front                 Right           Left                Back
+# 10    130            10 cm, 120      10 cm, 114          10, 130
+# 50    506            50 cm, 506      50 cm, 507          50, 517-518
+# 100   1007          100,1015        100, 1015           100, 1025-1028
+# 150   1485          150,1494        150, 1493           150, 1487
+# 200   1985   200,1994        200, 1988           200, 1993
+# 250   2472          250,2479        250, 2480           250, 2476
+# 300   2971          300,2973        300, 2970           300, 2975 or 2608
 
 # Lav gennemsnit
