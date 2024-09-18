601f6db62ef5068e4cae7a9917a8b377e84685f5
norbjerg <rzd350@alumni.ku.dk>
2024-09-18 14:08:11 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,7 +1,53 @@
-from picamera2 import Picamera2
-from picamera2 import Preview
 import time
-from landmark import cam_on, detect
+
+import cv2
+from picamera2 import Picamera2
+
+import robot
+
+arlo = robot.Robot()
+
+speed = 40
+tspeed_slow = 32
+tspeed = 32
+aruco = False
+image = "aruco.png"
+arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+preview_downscale = 2
+imageSize = (1920 // 2**preview_downscale, 1080 // 2**preview_downscale)
+center_image = (imageSize[0] // 2, imageSize[1] // 2)
+FPS = 5
+frame_duration_limit = int(1 / FPS * 1000000)  # Microseconds
+
+
+picam2 = Picamera2()
+picam2_config = picam2.create_video_configuration(
+    {"size": imageSize, "format": "RGB888"},
+    controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+    queue=False,
+)
+picam2.configure(picam2_config)  # Not really necessary
+picam2.start(show_preview=False)
+time.sleep(2)
+picam2.start()
+# cap = cv2.VideoCapture()
+
+arucoParams = cv2.aruco.DetectorParameters()
+
+
+def detect(image_inp):
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(
+        image=image_inp, dictionary=arucoDict, parameters=arucoParams
+    )
+    return corners, ids, rejected
+
+def cam_on():
+    while True:
+        im = picam2.capture_array()
+
+        grey = cv2.cvtColor(im, cv2.COLOR_RGB2RGBA)
+        return grey
+
 
 markerDist = int(input("Distance from marker: "))
 markerHeight = 50  # mm
@@ -56,5 +102,6 @@ for i in range(100):
     inp = input("name (q for quit): ").replace(" ", "_")
     if inp.lower() == "q":
         exit()
+    get_marker_dim()
     picam2.capture_file(f"img{i}_{inp}.jpg")
 
