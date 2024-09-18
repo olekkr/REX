f2df82c74880b3b9990855d65f4bd2dabb4b4a07
norbjerg <rzd350@alumni.ku.dk>
2024-09-18 10:46:00 +0200

    Update landmark.py

--- landmark.py
+++ landmark.py
@@ -6,7 +6,6 @@ from picamera2 import Picamera2
 import robot
 
 arlo = robot.Robot()
-# cap = cv2.VideoCapture(0)
 
 speed = 40
 tspeed = 32
@@ -19,7 +18,7 @@ camera_config = picam2.create_still_configuration()
 picam2.configure(camera_config)
 time.sleep(2)
 picam2.start()
-cap = picam2.capture()
+cap = cv2.VideoCapture(0)
 
 
 def drive_straight():
