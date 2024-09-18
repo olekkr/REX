8f7f2fc214ee9d9ae3aff5324a6c43137f70effa
norbjerg <rzd350@alumni.ku.dk>
2024-09-18 11:53:27 +0200

    Update landmark.py

--- landmark.py
+++ landmark.py
@@ -77,30 +77,86 @@ def cam_on():
 
 
 while 1:
-    
-    (corners, ids, rejected) = detect(cam_on())
+    image = cam_on()
+    (corners, ids, rejected) = detect(image)
     front = arlo.read_sensor(0)
     lefts = arlo.read_sensor(2)
     rights = arlo.read_sensor(3)
 
-    qr_leftdown = corners[0][0]
-    qr_rightdown = corners[0][1]
+    # verify *at least* one ArUco marker was detected
+    if len(corners) > 0:
+        # flatten the ArUco IDs list
+        ids = ids.flatten()
+        # loop over the detected ArUCo corners
+        for (markerCorner, markerID) in zip(corners, ids):
+            # extract the marker corners (which are always returned in
+            # top-left, top-right, bottom-right, and bottom-left order)
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
 
-    # TESTING
-    print("LD: ", qr_leftdown)
-    print("RD: ", qr_rightdown)
+            # draw the bounding box of the ArUCo detection
+            cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            # compute and draw the center (x, y)-coordinates of the ArUco
+            # marker
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+            # draw the ArUco marker ID on the image
+            cv2.putText(image, str(markerID),
+                (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+            print("[INFO] ArUco marker ID: {}".format(markerID))
+            # show the output image
+            cv2.imshow("Image", image)
+            cv2.waitKey(0)
 
-    qr_leftUp = corners[1][0]
-    qr_rightUp = corners[1][1]
-    print("LU: ", qr_leftUp)
-    print("RU: ", qr_rightUp)
+    # qr_leftdown = bottomLeft
+    # qr_rightdown = bottomRight
 
-    middle = (qr_leftdown + qr_rightdown) / 2
-    close_middle = [middle - 50, middle + 50]
+    # # TESTING
+    # print("LD: ", qr_leftdown)
+    # print("RD: ", qr_rightdown)
 
-    if middle > close_middle[0] and middle < close_middle[1]:
-        drive_straight()
-    elif qr_leftdown < middle and qr_rightdown < middle:
-        turn_right()
-    else:
-        turn_left()
+    # qr_leftUp = topLeft
+    # qr_rightUp = topRight
+    # print("LU: ", qr_leftUp)
+    # print("RU: ", qr_rightUp)
+
+    # # draw the bounding box of the ArUCo detection
+    # cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+    # cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+    # cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+    # cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+
+
+    # # compute and draw the center (x, y)-coordinates of the ArUco
+    # # marker
+    # cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+    # cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+    # cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+    # # draw the ArUco marker ID on the image
+    # cv2.putText(image, str(markerID),
+    #     (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+    #     0.5, (0, 255, 0), 2)
+    # print("[INFO] ArUco marker ID: {}".format(markerID))
+    # # show the output image
+    # cv2.imshow("Image", image)
+    # cv2.waitKey(0)
+
+    # middle = (qr_leftdown + qr_rightdown) / 2
+    # close_middle = [middle - 50, middle + 50]
+
+    # if middle > close_middle[0] and middle < close_middle[1]:
+    #     drive_straight()
+    # elif qr_leftdown < middle and qr_rightdown < middle:
+    #     turn_right()
+    # else:
+    #     turn_left()
