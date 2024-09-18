b042c1c2ce07807b7194dc16030bbbdea9af482e
norbjerg <rzd350@alumni.ku.dk>
2024-09-18 16:34:59 +0200

    Update landmark.py

--- landmark.py
+++ landmark.py
@@ -115,56 +115,6 @@ while 1 and __name__ == "__main__":
 
     # verify *at least* one ArUco marker was detected
     if len(corners) > 0:
-        # flatten the ArUco IDs list
-        # print("id", ids)
-        # print("corners", corners)
-        # ids = ids.flatten()
-        # loop over the detected ArUCo corners
-        # for markerCorner, markerID in zip(corners, ids):
-        #     # extract the marker corners (which are always returned in
-        #     # top-left, top-right, bottom-right, and bottom-left order)
-        #     corners = markerCorner.reshape((4, 2))
-        #     (topLeft, topRight, bottomRight, bottomLeft) = corners
-        #     # convert each of the (x, y)-coordinate pairs to integers
-        #     topRight = (int(topRight[0]), int(topRight[1]))
-        #     bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-        #     bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-        #     topLeft = (int(topLeft[0]), int(topLeft[1]))
-        #     # draw the bounding box of the ArUCo detection
-        #     cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-        #     cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-        #     cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-        #     cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
-        #     # compute and draw the center (x, y)-coordinates of the ArUco
-        #     # marker
-        #     cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-        #     cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-        #     cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
-        #     # draw the ArUco marker ID on the image
-        #     cv2.putText(
-        #         image,
-        #         str(markerID),
-        #         (topLeft[0], topLeft[1] - 15),
-        #         cv2.FONT_HERSHEY_SIMPLEX,
-        #         0.5,
-        #         (0, 255, 0),
-        #         2,
-        #     )
-        #     print("[INFO] ArUco marker ID: {}".format(markerID))
-        # print(
-        #     len(corners),
-        #     corners,
-        #     CameraMatrix,
-        #     DistortionCoefficient,
-        #     sep="\n",
-        # )
-        #     cv2.aruco.estimatePoseSingleMarkers(
-        #         corners,
-        #         markerHeight,
-        #         CameraMatrix(preview_downscale),
-        #         DistortionCoefficient,
-        #     )
-        # )
         print(corners)
         a, b, c = cv2.aruco.estimatePoseSingleMarkers(
             corners,
@@ -172,14 +122,14 @@ while 1 and __name__ == "__main__":
             CameraMatrix(preview_downscale),
             DistortionCoefficient,
         )
+        print(a)
     try:
         for corner in corners:
             cv2.imshow("Image", cv2.aruco.drawDetectedCornersCharuco(image, corner))
     except Exception as e:
         print(e)
         cv2.imshow("Image", image)
-        # print(a, b, c)
-    # show the output image
+
     cv2.waitKey(1)
     if cv2.getWindowProperty("Image", 0) == -1:
         arlo.stop()
