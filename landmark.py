bc99768f2941035e89af42f5c2a2aa2830262c94
norbjerg <rzd350@alumni.ku.dk>
2024-09-18 12:04:30 +0200

    Update landmark.py

--- landmark.py
+++ landmark.py
@@ -120,7 +120,7 @@ while 1:
             print("[INFO] ArUco marker ID: {}".format(markerID))
             # show the output image
             cv2.imshow("Image", image)
-            cv2.waitKey(1)
+            # cv2.waitKey()
 
     # qr_leftdown = bottomLeft
     # qr_rightdown = bottomRight
