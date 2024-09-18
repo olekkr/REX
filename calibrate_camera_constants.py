219a3612a5d0e1b18eb3e1e66003d4486df875ad
norbjerg <rzd350@alumni.ku.dk>
2024-09-18 14:56:34 +0200

    Update calibrate_camera_constants.py

--- calibrate_camera_constants.py
+++ calibrate_camera_constants.py
@@ -1,17 +1,19 @@
 import numpy as np
 
-FocalLength = 1880
+FocalLength = 1880.
 
-markerHeight = 50 # mm
+markerHeight = 50. # mm
 
 CameraMatrix = lambda downscale: np.matrix(
     [
-        [FocalLength, 0, 1920 // (2**downscale)],
-        [0, FocalLength, 1080 // (2**downscale)],
+        [FocalLength, 0., 1920 / (2**downscale)],
+        [0., FocalLength, 1080. // (2**downscale)],
         [0, 0, 1],
-    ]
+    ],
+    dtype=float
 )
 
-DistortionCoefficient = np.array(
-    [0,0,0,0]
+DistortionCoefficient = np.matrix(
+    [[0,0,0,0]],
+    dtype=float
 )
