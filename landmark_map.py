fc31d548fb12e2011e2c1ae614b1f9f1a6ed8ecd
norbjerg <rzd350@alumni.ku.dk>
2024-09-23 15:44:19 +0200

    Update landmark_map.py

--- landmark_map.py
+++ landmark_map.py
@@ -94,14 +94,16 @@ def cam_on():
         cv2.waitKey(1)
 
 
-map_x, map_y = ([0, -1000, 1000], [0, 0, 1000])
+map_x, map_y = ([], [])
 fig, ax = plt.subplots()
 
 
 def update(frame):
     plt.draw()
     ax.clear()  # clearing the axes
-    ax.scatter(map_x, map_y, s=map_y, c="b", alpha=0.5)  # creating new scatter chart with updated data
+    ax.scatter(
+        map_x, map_y, s=map_y, c="b", alpha=0.5, vmin=-1000, vmax=1000
+    )  # creating new scatter chart with updated data
     fig.canvas.draw()  # forcing the artist to redraw itself
 
 
