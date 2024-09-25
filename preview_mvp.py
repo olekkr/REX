711d2f7f625f02c89d5158263630a5cf3a2c08d0
ShatinNguyen <shatin2@hotmail.com>
2024-09-25 15:28:01 +0200

    asdasd

--- preview_mvp.py
+++ preview_mvp.py
@@ -5,8 +5,8 @@ import constants
 
 picam2 = camera_setup.camera_setup()
 
-image = picam2.capture_array("main")
-
-if constants.ENABLE_PREVIEW:
-    cv2.waitKey(1)
-    cv2.imshow("Image", image)
+while 1:
+    image = picam2.capture_array("main")
+    if constants.ENABLE_PREVIEW:
+        cv2.waitKey(1)
+        cv2.imshow("Image", image)
