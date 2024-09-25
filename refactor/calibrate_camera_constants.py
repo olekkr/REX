64d6b0db43ad083c700758c7658a93cd296f4bad
norbjerg <rzd350@alumni.ku.dk>
2024-09-25 17:12:34 +0200

    cco

---
+++ refactor/calibrate_camera_constants.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+downscale_factor = 1
+
+dimensions = (int(1920 // 2**downscale_factor), int(1080 // 2**downscale_factor))
+
+# FocalLength = 620.0
+FocalLengthArr = [2540, 1270, 625.0]
+FocalLength = FocalLengthArr[downscale_factor] if len(FocalLengthArr) > downscale_factor else FocalLengthArr[0] / 2**downscale_factor
+
+markerHeight = 145.0  # mm
+
+
+CameraMatrix = np.array(
+    [
+        [FocalLength, 0, dimensions[0] / 2],
+        [0, FocalLength, dimensions[1] / 2],
+        [0, 0, 1],
+    ],
+    dtype=float,
+)
+
+DistortionCoefficient = np.array([0, 0, 0, 0, 0], dtype=float)
+
+print("using dimention:", dimensions)
+print("using FocalLength:", FocalLength)
--- refactor/constants.py
+++ refactor/constants.py
@@ -5,3 +5,24 @@ WHEEL_DIAMETER = 15.5 # cm
 WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER*3.14  # cm
 ROBOT_CIRCUMFERENCE = 39.5*3.14  # cm
 QUARTER_TURN_64 = 0.725 # sleep
+
+
+from calibrate_camera_constants import FocalLength, CameraMatrix, dimensions, downscale_factor
+
+
+#FOCALLENGTH = 625.0
+FOCALLENGTH = FocalLength
+MARKER_HEIGHT = 145.0
+DISTORTION_COEFFICIENT = np.array([0, 0, 0, 0, 0], dtype=float)
+
+
+# ON PId
+DOWNSCALE = downscale_factor
+SCREEN_RESOLUTION = dimensions
+CAMERA_MATRIX = CameraMatrix
+
+
+PREVIEW_DOWNSCALE = 2
+ENABLE_PREVIEW = 1
+CAMERA_FPS = 24
+
