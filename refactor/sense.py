84aa04cc3fc0dba91d2e261832044f6805fc4573
olekkr <olekkr@outlook.com>
2024-09-25 15:17:44 +0200

    first sense.py

---
+++ refactor/sense.py
@@ -0,0 +1,72 @@
+import numpy as np 
+import ../constants
+
+
+dimensions = (1920, 1080)
+
+FocalLength = 2540
+
+markerHeight = 145.0  # mm
+FPS = 5
+
+
+
+CamCx, CamCy = dimensions[0]/2, dimensions[1]/2
+CameraMatrix = np.array(
+    [
+        [FocalLength, 0, CamCx / 2],
+        [0, FocalLength, CamCy / 2],
+        [0, 0, 1],
+    ],
+    dtype=float,
+)
+DistortionCoefficient = np.array([0, 0, 0, 0, 0])
+
+print("using dimention:", dimensions)
+print("using FocalLength:", FocalLength)
+
+
+def init():
+    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+    arucoParams = cv2.aruco.DetectorParameters()
+
+
+    # pi cam init
+    frame_duration_limit = int(1 / FPS * 1000000)  # Microseconds
+    picam2 = Picamera2()
+    picam2_config = picam2.create_video_configuration(
+        {"size": imageSize, "format": "RGB888"},
+        controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+        queue=False,
+    )
+    picam2.configure(picam2_config)  # Not really necessary
+    picam2.start(show_preview=False)
+    time.sleep(2)
+    picam2.start()
+
+def sense(grid): # map, 
+    sense_camera(grid)
+
+
+
+
+
+def sense_camera(grid): 
+    # capture RGB:
+    im = picam2.capture_array("main")
+
+    # capture AruCo Corners 
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(image=image_inp, dictionary=arucoDict)
+    
+    # get Markers in camera coordinate system
+    _rt, tv, _objs = cv2.aruco.estimatePoseSingleMarkers(
+        corners, markerHeight, CameraMatrix, DistortionCoefficient,
+        )
+    
+    for t in tv:
+        draw_landmarks(t, grid)
+
+    
+
+
+
