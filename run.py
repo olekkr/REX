10d516d60c5a6b6fb6f9dfb053a0872000d1f973
ShatinNguyen <shatin2@hotmail.com>
2024-09-25 15:03:06 +0200

    hejsa

--- run.py
+++ run.py
@@ -17,9 +17,9 @@ frame_duration_limit = int(1 / FPS * 1000000)  # Microseconds
 
 picam2 = Picamera2()
 picam2_config = picam2.create_video_configuration(
-    {"size": (1640,922), "format": "RGB888"},
+    {"size": (1640,1232), "format": "RGB888"},
     controls={
-         "ScalerCrop": (0, 0, 1640, 922),
+         "ScalerCrop": (0, 0, 1640, 1232),
          "FrameDurationLimits": (frame_duration_limit, frame_duration_limit)}, 
          queue=False,
     )
