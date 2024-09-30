6927c453fcf54658621644794477e71224194a59
norbjerg <rzd350@alumni.ku.dk>
2024-09-30 16:14:30 +0200

    Update local_planning.py

--- refactor/local_planning.py
+++ refactor/local_planning.py
@@ -12,7 +12,7 @@ from constants import Constants
 
 # AN occupancy grid implementation
 
-GRID_FIDELITY = Constants.Robot.DIAMETER  # size of each grid square in mm
+GRID_FIDELITY = 100  # size of each grid square in mm
 LANDMARK_DIAMETER = 450
 
 
