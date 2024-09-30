4e50fba9c4186d13492dc9d3bad926979ca6f792
norbjerg <rzd350@alumni.ku.dk>
2024-09-30 15:04:45 +0200

    Update sense.py

--- refactor/sense.py
+++ refactor/sense.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
             print("plot closed, exiting...")
             exit()
         grid = sense(grid)
-        if time.time() - start > 5:
+        if time.time() - start > 1:
             plt.pause(0.0001)
             local_planning.show_grid(grid, robo_pos, axes)
             start = time.time()
