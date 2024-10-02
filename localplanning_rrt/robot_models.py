160995479b929d214f1eff2adb8cc30c2fece3e2
norbjerg <rzd350@alumni.ku.dk>
2024-10-02 10:47:21 +0200

    Update robot_models.py

--- localplanning_rrt/robot_models.py
+++ localplanning_rrt/robot_models.py
@@ -12,31 +12,45 @@ from constants import Constants
 
 class RobotModel:
 
-'    def __init__(self, ctrl_range) -> None:
+    def __init__(self, ctrl_range) -> None:
         #record the range of action that can be used to integrate the state
         self.ctrl_range = ctrl_range
-        return'
-    
+        return
+
     def forward_dyn(self, x, u, T):
-        #need to return intergated path of X with u along the horizon of T
         path = [x]
         for i in range(T):
             v, w = u[i]
+            theta = np.arctan2(x[1], x[0])
 
-            x_new = path[-1] + np.array(v * np.cos(w), v * np.sin(w))
+            if w == 0:  # Straight motion
+                x_dot = v * np.cos(theta)
+                y_dot = v * np.sin(theta)
+            else:  # Rotation
+                x_dot = -v * np.sin(theta)
+                y_dot = v * np.cos(theta)
+
+            x_new = path[-1] + np.array([x_dot, y_dot])
             path.append(x_new)
+
         return path[1:]
 
     def inverse_dyn(self, x, x_goal, T):
-        #return dynamically feasible path to move to the x_goal as close as possible
+        u = []
+        for i in range(T):
+            dx = x_goal - x
+            theta = np.arctan2(dx[1], dx[0])
+            if np.linalg.norm(dx) > 1e-3:
+                v = self.ctrl_range[1]
+                w = 0
+            else:
+                v = 0
+                w = 2 * self.ctrl_range[1] / T
 
-        u = np.array([])
-        for _ in range(T):
-            pos = 
-            tx = self.ctrl_range[1]
+            u.append((v, w))
+            x = self.forward_dyn(x, [(v, w)], 1)[-1]
 
-
-        return self.forward_dyn(x, u, T)
+        return self.forward_dyn(x, u, 1)
 
 class PointMassModel(RobotModel):
     #Note Arlo is differential driven and may be simpler to avoid Dubins car model by rotating in-place to direct and executing piecewise straight path  
