45f9c5f69aacc5c9bfca4cfe0631755afa5c2a8a
norbjerg <rzd350@alumni.ku.dk>
2024-10-07 16:11:41 +0200

    Update selflocalize.py

--- ex5/selflocalize.py
+++ ex5/selflocalize.py
@@ -49,7 +49,7 @@ CBLACK = (0, 0, 0)
 
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 4]
+landmarkIDs = [1, 2]
 landmarks = {
     1: (0.0, 0.0),  # Coordinates for landmark 1
     2: (30.0, 0.0)  # Coordinates for landmark 2
