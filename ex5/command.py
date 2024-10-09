5cc015b1920553bea0ac62d7d6946757238a712d
norbjerg <120656170+norbjerg@users.noreply.github.com>
2024-10-09 14:15:19 +0200

    Update command.py

--- ex5/command.py
+++ ex5/command.py
@@ -48,9 +48,9 @@ class command:
             self.rotation_speed = ROTATIONAL_SPEED
             self.velocity = 0
             if self.angle > 0:
-                self.robot.diff(1, 0, 32, 32)
+                self.robot.go_diff(32, 32, 1, 0)
             else:
-                self.robot.diff(0, 1, 32, 32)
+                self.robot.go_diff(32, 32, 0, 1, )
 
         # has not started yet
         if self.startTime is None:
@@ -67,14 +67,14 @@ class command:
         elif time.time() - self.startTime < self.rotationTime + self.forwardTime:
             self.rotation_speed = 0
             self.velocity = ROTATIONAL_SPEED
-            self.robot.diff(1, 1, 64, 64)
+            self.robot.go_diff(64, 64, 1, 1)
             return False
 
         # has finished rotation and forward
         else:
             self.rotation_speed = 0
             self.velocity = 0
-            self.robot.diff(1, 1, 0, 0)
+            self.robot.go_diff(0, 0, 1, 1,)
 
             return True
         # True means finished
@@ -87,7 +87,7 @@ class controlWrapper:
         self.isArlo = isArlo
 
     ## TODO: do this for all the commands diff etc...
-    def diff(self, l, r, L, R):
+    def go_diff(self, l, r, L, R):
         if self.isArlo:
             self.robot.go_diff(l, r, L, R)
         else:
