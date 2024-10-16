58478dcaac4aa1a32ab6a09adbbbdb38fe5744b6
norbjerg <120656170+norbjerg@users.noreply.github.com>
2024-10-16 12:01:46 +0200

    Update selflocalize.py

--- ex5/selflocalize.py
+++ ex5/selflocalize.py
@@ -104,7 +104,8 @@ def particle_likelihood(particle, measurements):
             # print(f"alert: {l_id} seen and ignored")
             continue
         else:
-            print(f" {l_id} {m_dist} {np.rad2deg(m_ang)}")
+            # print(f" {l_id} {m_dist} {np.rad2deg(m_ang)}")
+            pass
 
         
         land_pos = np.array(landmarks[l_id])
