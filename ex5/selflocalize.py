69baf78a289459363b19842fdc5e8107c10e8a5a
norbjerg <120656170+norbjerg@users.noreply.github.com>
2024-10-16 11:58:40 +0200

    Update selflocalize.py

--- ex5/selflocalize.py
+++ ex5/selflocalize.py
@@ -210,7 +210,7 @@ try:
 
 
     # Initialize particles
-    num_particles = 1000
+    num_particles = 300
     particles = initialize_particles(num_particles)
 
     est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
