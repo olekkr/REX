ef97b9b9b504a15f012b054fcb72ea6fae4b469f
norbjerg <120656170+norbjerg@users.noreply.github.com>
2024-10-16 11:54:46 +0200

    Update selflocalize.py

--- ex5/selflocalize.py
+++ ex5/selflocalize.py
@@ -198,9 +198,9 @@ def initialize_particles(num_particles):
 try:
     if showGUI:
         # Open windows
-        WIN_RF1 = "Robot view"
-        cv2.namedWindow(WIN_RF1)
-        cv2.moveWindow(WIN_RF1, 50, 50)
+        # WIN_RF1 = "Robot view"
+        # cv2.namedWindow(WIN_RF1)
+        # cv2.moveWindow(WIN_RF1, 50, 50)
 
         WIN_World = "World view"
         cv2.namedWindow(WIN_World)
